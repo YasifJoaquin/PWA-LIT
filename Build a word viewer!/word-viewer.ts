17fee0b44b35f08306dbd572113188ae8da8b6ae
YasifJoaquin <yasif.19070002@itsmotul.edu.mx>
2023-02-09 12:33:39 -0600

    paso 4

--- Build a word viewer!/word-viewer.ts
+++ Build a word viewer!/word-viewer.ts
@@ -1,11 +1,14 @@
 import { html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 
 @customElement('word-viewer')
 class WordViewer extends LitElement {
+  // TODO: Add `idx` state
   @property() words = 'valor inicial';
 
   render() {
-    return html`<pre>${this.words}</pre>`
+    // TODO: Split the `words` by `'.'`, and from the resulting word array
+    // only show the word on index `this.idx`.
+    return html`<pre>${this.words}</pre>`;
   }
 }
