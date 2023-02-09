af4be2a3de820e08c04cff91a80ed023aff7bf34
YasifJoaquin <yasif.19070002@itsmotul.edu.mx>
2023-02-09 11:47:31 -0600

    paso 3

--- Working with Lists/my-element.ts
+++ Working with Lists/my-element.ts
@@ -1,17 +1,18 @@
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
-import {map} from 'lit/directives/map.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  items = new Set(['Spiderman 2', 'Sherk', 'Split', 'Sputnik', 'Lost'])
+  names = ['Yasif', 'Carla', 'Yair', 'Joel', 'Yamir', 'Ana'];
 
   render() {
     return html`
-      <p>Mi lista de peliculas: </p>
+      <p>Lista de nombres que tengan la letra "Y"</p>
       <ul>
-        ${map(this.items, (item) => html`<li>${item}</li>`)}
+      ${this.names
+        .filter((name) => name.match(/y/i))
+        .map((name) => html`<li>${name}</li>`)}
       </ul>
     `;
   }
