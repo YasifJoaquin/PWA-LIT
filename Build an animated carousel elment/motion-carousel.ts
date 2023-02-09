b50027bda8ed1a44e232b745cd40e83f27d11c27
YasifJoaquin <yasif.19070002@itsmotul.edu.mx>
2023-02-09 13:49:29 -0600

    paso 2

--- Build an animated carousel elment/motion-carousel.ts
+++ Build an animated carousel elment/motion-carousel.ts
@@ -1,53 +1,17 @@
-/* playground-fold */
-import {css} from 'lit';
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {styles} from './styles.js';
 
-export const styles = css`
-  :host {
-    display: inline-block;
-    overflow: hidden;
-    position: relative;
-    /* Defaults */
-    width: 200px;
-    height: 200px;
-    border-radius: 4px;
-    background: gainsboro;
-    cursor: pointer;
+@customElement('motion-carousel')
+export class MotionCarousel extends LitElement {
+  static styles = styles;
+
+  render() {
+    return html`
+      <div class="fit">
+        <slot></slot>
+      </div>
+    `;
   }
 
-  .fit {
-    position: relative;
-    height: 100%;
-    width: 100%;
-  }
-
-  .selected {
-    top: -100%;
-  }
-
-  ::slotted(*) {
-    box-sizing: border-box;
-    width: 100%;
-    height: 100%;
-  }
-
-  .bar {
-    position: absolute;
-    bottom: 8px;
-    width: calc(100% - 16px);
-    left: 8px;
-    height: 8px;
-    background: rgba(200, 200, 200, 0.5);
-    border-radius: 8px;
-    pointer-events: none;
-  }
-
-  .indicator {
-    position: relative;
-    display: inline-block;
-    height: 100%;
-    width: 8px;
-    border-radius: 8px;
-    background: #eee;
-  }`;
-
-/* playground-fold-end */
+}
