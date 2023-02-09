caca6e83167e37d2ba35808ab2ac47d89c3c1920
YasifJoaquin <yasif.19070002@itsmotul.edu.mx>
2023-02-09 10:36:01 -0600

    paso 2

--- Intro to Lit/my-element.ts
+++ Intro to Lit/my-element.ts
@@ -1,15 +1,11 @@
 import {LitElement, html} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement} from 'lit/decorators.js';
 
-@customElement('my-element')
+@customElement('mi-elemento')
 export class MyElement extends LitElement {
-  @property()
-  version = 'INICIO';
-
   render() {
     return html`
-    <p>Welcome to the Lit tutorial!</p>
-    <p>Este es el ${this.version} del Tutorial.</p>
+      <p>Hola a todos! Desde "mi-elemento".</p>
     `;
   }
 }
