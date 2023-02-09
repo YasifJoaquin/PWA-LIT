c19167a2078ad081392fe71752082c0bef10e0e6
YasifJoaquin <yasif.19070002@itsmotul.edu.mx>
2023-02-09 10:44:03 -0600

    paso 8

--- Intro to Lit/my-element.ts
+++ Intro to Lit/my-element.ts
@@ -1,10 +1,10 @@
 import {LitElement, html, css} from 'lit';
-import {customElement, state, query} from 'lit/decorators.js';
+import {customElement, state, property, query} from 'lit/decorators.js';
 
 type ToDoItem = {
   text: string,
   completed: boolean
-}
+};
 
 @customElement('todo-list')
 export class ToDoList extends LitElement {
@@ -20,28 +20,56 @@ export class ToDoList extends LitElement {
     { text: 'Realizar el tutoria Lit', completed: true },
     { text: 'Crear lista de deberes', completed: false }
   ];
+  @property()
+  hideCompleted = false;
 
   render() {
-    return html`
-      <h2>To Do</h2>
+    const items = this.hideCompleted
+      ? this._listItems.filter((item) => !item.completed)
+      : this._listItems;
+    const todos = html`
       <ul>
-        ${this._listItems.map((item) =>
-          html`
-            <li
-                class=${item.completed ? 'completed' : ''}
-                @click=${() => this.toggleCompleted(item)}>
-              ${item.text}
-            </li>`
+        ${items.map((item) =>
+            html`
+              <li
+                  class=${item.completed ? 'completed' : ''}
+                  @click=${() => this.toggleCompleted(item)}>
+                ${item.text}
+              </li>`
         )}
       </ul>
+    `;
+    const caughtUpMessage = html`
+      <p>
+      You're all caught up!
+      </p>
+    `;
+    const todosOrMessage = items.length > 0
+      ? todos
+      : caughtUpMessage;
+
+    return html`
+      <h2>Deberes</h2>
+      ${todosOrMessage}
       <input id="newitem" aria-label="New item">
       <button @click=${this.addToDo}>Add</button>
+      <br>
+      <label>
+        <input type="checkbox"
+          @change=${this.setHideCompleted}
+          ?checked=${this.hideCompleted}>
+        Hide completed
+      </label>
     `;
   }
 
   toggleCompleted(item: ToDoItem) {
     item.completed = !item.completed;
     this.requestUpdate();
+  }
+
+  setHideCompleted(e: Event) {
+    this.hideCompleted = (e.target as HTMLInputElement).checked;
   }
 
   @query('#newitem')
